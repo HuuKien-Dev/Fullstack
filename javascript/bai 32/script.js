d9e22d9f123bdc7321c5c5a2858508b61dd3020f
Huu Kien <kien.dev.uet@gmail.com>
2026-04-20 21:20:43 +0700

    b1 javascript

---
+++ javascript/bai 32/script.js
@@ -0,0 +1,68 @@
+// // Variable (Bien)
+// var a = 10;
+// var b = 20;
+// var c = a + b;
+// console.log(c);
+
+// // Toan tu so hoc + - * /
+// var a = 2;
+// var b = 3;
+// var c = a**b;
+// console.log(c);
+// console.log (a/b);
+// console.log(a*b);
+// console.log(a**b);
+// console.log(a-b);
+// console.log(a+b);
+// console.log(a%b);
+
+// Toan tu gan
+
+// // Toan tu so sanh === va !==
+// var a = 0;
+// var b = 2;
+// console.log(a > b);
+// console.log ( a <= b);
+// var c = '2';
+// console.log(c == b); // true - so sanh ko nghiem ngat 
+// console.log(c === b) // false - so sanh nghiem ngat phai giong nhau ca ve gia tri va kieu du lieu
+// console.log(b != c);// fale
+// console.log (b !== c);// true
+
+// Toan tu logic && || ! and or not
+
+// Kieu du lieu nguyen thuy
+// number - so nguyen -so thuc
+// 3 so dac biet Infinity , -Infinity, naN
+// string " ";'';``.
+// var fullName = `Le Van A`;
+// var phone = `0192928728`;
+// var messages1 = `xin chao ${fullName} ${phone}`;
+// console.log(messages1);
+
+// Kieu du lieu phuc tap
+
+// Ham function
+
+// function Sum(a,b)
+// {
+//     return a + b;
+// }
+ 
+// var s = Sum(10,20);
+// console.log(s);
+
+// var s2 = Sum(1, 2);
+// console.log(s2);
+
+// Kieu du lieu object -chia 2 loai mang va doi tuong
+// var InforUser = {
+//     fullName: "Le Van A",
+//     address: "So 123 duong A thanh pho B",
+//     age: 20 ,
+//     isOnline: true,
+// };
+// console.log(InforUser);
+ 
+// var list=[10,20,30,30,40,50];
+// console.log(list);
