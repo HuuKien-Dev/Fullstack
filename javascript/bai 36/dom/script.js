75500a83a7626eee45f2709e12936cb4099f0c0b
Huu Kien <kien.dev.uet@gmail.com>
2026-05-03 07:58:04 +0700

    Dom

--- javascript/bai 36/dom/script.js
+++ javascript/bai 36/dom/script.js
@@ -1,5 +1,34 @@
+// DOM element
+
 const buttonCreate = document.querySelector(".section-1 .inner-create");
 console.log(buttonCreate);
 
 const buttonDelete = document.querySelectorAll(".section-1 .inner-delete");
 console.log(buttonDelete);
+
+// DOM html
+
+const title = document.querySelector(".title");
+console.log(title.innerHTML);
+title.innerHTML = "Tieu de duoc thay doi";
+console.log(title.innerHTML);
+
+// Dom csss
+
+
+const title1 = document.querySelector(".title");
+title1.style.color = "red";
+title1.style.fontSize = "40px";
+title1.style.fontWeight = "700";
+title1.style.textAlign = "center";
+
+// DOM event 
+
+const search = document.querySelector(".input-search");
+search.onfocus = () => {
+    console.log("Da co ng click vo o tim kiem");
+}
+
+search.onblur = () => {
+    console.log("Thoat khoi su kien nhap");
+}
